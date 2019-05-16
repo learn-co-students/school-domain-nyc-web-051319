df4640e5c0fd7c247d4ca2021defa566af78b454
Yana <Yana.Loginova@cix.csi.cuny.edu>
2019-05-15 20:54:18 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+  attr_accessor :roster,:name,:grade
+
+def initialize(arg)
+  @roster = {}
+  # @grade=grade
+end
+
+def add_student(name,grade)
+  @roster[grade] ||=[]
+  @roster[grade]<<name
+end
+def grade(grade)
+  @roster.each do |key, value|
+    if key == grade
+      return value
+end
+end
+end
+def sort
+
+  @roster.each do |key,value|
+    @roster[key]=value.sort
+end
+end
+end
